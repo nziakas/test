e3c75112289d74d5a464d7aa3d30aaf03d55c2f8
nziakas <nziakas@gmail.com>
2023-04-24 02:50:08 +0300

    testing new function

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def my_function(a, b):
+    return a + b
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -11,3 +14,5 @@ if __name__ == '__main__':
     print('How are you')
     print('How are you')
     print(password)
+
+print(my_function(2, 4))
